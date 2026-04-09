95ce813773ef94a4badde1d54fa9902a0839a956
Antony Perez <aperez-2023350@kinal.edu.gt>
2026-04-09 12:00:11 -0600

    001 Se agregaron opciones al menu prinsipal

--- src/org/antonyperez/view/ViewMenuPrinsipal.java
+++ src/org/antonyperez/view/ViewMenuPrinsipal.java
@@ -11,6 +11,11 @@ public class ViewMenuPrinsipal {
     public void mostraMenu(){
         while (opcionMenu == 0){
         System.out.println("Bienvenido al menu, Seleccione una opcion");
+        System.out.println("1. Operaciones Matematicas");
+        System.out.println("2. Operaciones de texto");
+        System.out.println("3. Operaciones de Logica");
+        System.out.println("4. Salir de del sistema");
+       
         opcionMenu = Integer.parseInt(sc.nextLine());
         }
     }
