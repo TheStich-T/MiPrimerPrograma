a64d5a92509d7240bd18d6923dd38fa295e6a581
TheStich-T <aperez-2023350@kinal.edu.gt>
2026-04-12 22:38:09 -0600

    ControllerTexto.java
    006 Aquí se hicieron cambios en el Controllador Texto

--- src/org/antonyperez/controller/ControllerTexto.java
+++ src/org/antonyperez/controller/ControllerTexto.java
@@ -1,81 +1,83 @@
 package org.antonyperez.controller;
 import java.util.Scanner;
-/**
- *
- * @author Antony Perez - 2023350
- */
 public class ControllerTexto {
 Scanner sc = new Scanner(System.in);
     public void mostrarMenu() {
             int opcion;
             
             do {
-                System.out.println("===== MENÚ DE EJERCICIOS =====");
-                System.out.println("11. Concatenador de Nombre");
-                System.out.println("12. Contador de Longitud");
-                System.out.println("13. Inversor de Texto");
-                System.out.println("14. Detector de Vocales");
-                System.out.println("15. Extractor de Iniciales");
-                System.out.println("16. Normalizador a Mayúsculas");
-                System.out.println("17. Conversor Binario (par o impar)");
-                System.out.println("18. Repetidor de Palabras");
-                System.out.println("19. Validador de Password");
-                System.out.println("20. Generador de Email");
+                System.out.println("---- MENU DE EJERCICIOS ----");
+                System.out.println("1. Concatenador de Nombre");
+                System.out.println("2. Contador de Longitud");
+                System.out.println("3. Inversor de Texto");
+                System.out.println("4. Detector de Vocales");
+                System.out.println("5. Extractor de Iniciales");
+                System.out.println("6. Devolvedor a Mayusculas");
+                System.out.println("7. Conversor Binario (par o impar)");
+                System.out.println("8. Repetidor de Palabras");
+                System.out.println("9. Validador de Password");
+                System.out.println("10. Generador de Email");
                 System.out.println("0. Salir");
-                System.out.print("Seleccione una opción: ");
+                System.out.print("Seleccione una opcion: ");
                 
                 opcion = sc.nextInt();
                 sc.nextLine();
                 
                 switch(opcion) {
-                    case 11 -> {
-                        System.out.print("Ingrese nombre: ");
+                    case 1 -> {
+                        System.out.println("Vamos a concatenar");
+                        System.out.print("Ingrese su nombre: ");
                         String nombre = sc.nextLine();
-                        System.out.print("Ingrese apellido: ");
+                        System.out.print("Ingrese su apellido: ");
                         String apellido = sc.nextLine();
                         System.out.println("Nombre completo: " + nombre + " " + apellido);
                     }
                     
-                    case 12 -> {
-                        System.out.print("Ingrese una palabra: ");
+                    case 2 -> {
+                        System.out.println("Vamos a contar las palabras");
+                        System.out.print("Ingresa una palabra: ");
                         String palabra = sc.nextLine();
                         System.out.println("Longitud: " + palabra.length());
                     }
                     
-                    case 13 -> {
-                        System.out.print("Ingrese una palabra: ");
+                    case 3 -> {
+                        System.out.println("Vamos a invertir las palabras");
+                        System.out.print("Ingresa una palabra: ");
                         String texto = sc.nextLine();
                         String invertido = "";
                         for (int i = texto.length() - 1; i >= 0; i--) {
                             invertido += texto.charAt(i);
                         }
-                        System.out.println("Texto invertido: " + invertido);
+                        System.out.println("El texto invertido: " + invertido);
                     }
                     
-                    case 14 -> {
+                    case 4 -> {
+                        System.out.println("Vamos a ver si es una vocal");
                         System.out.print("Ingrese un carácter: ");
                         char letra = sc.next().charAt(0);
                         boolean esVocal = "aeiouAEIOU".indexOf(letra) != -1;
                         System.out.println("¿Es vocal?: " + esVocal);
                     }
                     
-                    case 15 -> {
-                        System.out.print("Ingrese nombre: ");
+                    case 5 -> {
+                        System.out.println("Vamos a extrar las inciales ");
+                        System.out.print("Ingresa tu nombre: ");
                         String nom = sc.nextLine();
-                        System.out.print("Ingrese apellido: ");
+                        System.out.print("Ingresa tu apellido: ");
                         String ape = sc.nextLine();
                         String iniciales = "" + nom.charAt(0) + ape.charAt(0);
                         System.out.println("Iniciales: " + iniciales.toUpperCase());
                     }
                     
-                    case 16 -> {
+                    case 6 -> {
+                        System.out.println("Te lo devolvemos en Mayusculos lo que ingreses");
                         System.out.print("Ingrese texto: ");
                         String txt = sc.nextLine();
                         System.out.println("En mayúsculas: " + txt.toUpperCase());
                     }
                     
-                    case 17 -> {
-                        System.out.print("Ingrese texto binario: ");
+                    case 7 -> {
+                        System.out.print("Ingresa un texto binario: ");
                         String binario = sc.nextLine();
                         if (binario.length() % 2 == 0) {
                             System.out.println("Longitud PAR");
@@ -84,10 +86,10 @@ Scanner sc = new Scanner(System.in);
                         }
                     }
                     
-                    case 18 -> {
-                        System.out.print("Ingrese palabra: ");
+                    case 8 -> {
+                        System.out.print("Ingresa una palabra: ");
                         String rep = sc.nextLine();
-                        System.out.print("Ingrese cantidad: ");
+                        System.out.print("Ingresa una cantidad: ");
                         int n = sc.nextInt();
                         for (int i = 0; i < n; i++) {
                             System.out.print(rep + " ");
@@ -95,8 +97,9 @@ Scanner sc = new Scanner(System.in);
                         System.out.println();
                     }
                     
-                    case 19 -> {
-                        System.out.print("Ingrese contraseña: ");
+                    case 9 -> {
+                        System.out.println("Veamos si es una buena contraseña");
+                        System.out.print("Ingresa una contraseña: ");
                         String pass = sc.nextLine();
                         if (pass.length() > 8) {
                             System.out.println("Segura");
@@ -105,12 +108,13 @@ Scanner sc = new Scanner(System.in);
                         }
                     }
                     
-                    case 20 -> {
-                        System.out.print("Ingrese nombre: ");
+                    case 10 -> {
+                        System.out.println("Creamos un correo para ti");
+                        System.out.print("Ingrese su nombre: ");
                         String n1 = sc.nextLine().toLowerCase();
-                        System.out.print("Ingrese apellido: ");
+                        System.out.print("Ingrese su apellido: ");
                         String a1 = sc.nextLine().toLowerCase();
-                        System.out.print("Ingrese dominio (ej: gmail.com): ");
+                        System.out.print("Ingrese un dominio (ej: gmail.com): ");
                         String dominio = sc.nextLine();
                         String email = n1 + "." + a1 + "@" + dominio;
                         System.out.println("Email generado: " + email);
